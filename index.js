5e22d7a33522fc2be692400e4540ef94b20d2735
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-03 12:46:32 +0700

    connect to mongodb

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+require('./db')
+
 app.get('/api/messages', require('./controllers/get_messages'))
+app.post('/api/messages', require('./controllers/post_messages'))
 
 app.listen(process.env.PORT, (err) => {
     if (err) {
     console.log('ERROR', err)
     } else {
-    console.log(`Connected to localhost ${process.env.PORT}`)
+    console.log(`Connected to localghost ${process.env.PORT}`)
     }
 })
