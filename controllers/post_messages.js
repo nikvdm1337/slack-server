a36397d1314513d2f546c72e8a167de7553b9a63
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-03 13:10:17 +0700

    created message structure

--- controllers/post_messages.js
+++ controllers/post_messages.js
@@ -1,3 +1,13 @@
+const db_message = require('../models/message')
+
 module.exports = (req, res) => {
-    res.send('Hhuehueheuheuehu')
+    db_message.create({
+        author: "Nikola",
+        body: "Hey Mongo!",
+        date: "03.07.2019"
+    }).then ((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err)
+    })
 }
---
+++ models/message.js
@@ -0,0 +1,9 @@
+const db = require('../db')
+
+const db_message = db.model('message', {
+    author: String,
+    date: String,
+    body: String,
+})
+
+module.exports = db_message
